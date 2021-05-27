fcb9289075fe15d878dc4e43128b8710f4a010af
FelipeVerde <>
2021-05-27 18:08:41 -0300

    Aula9 - java

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author felip
+ */
+public class Produto {
+     public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the valorUnitario
+     */
+    public String getValorUnitario() {
+        return valorUnitario;
+    }
+
+    /**
+     * @param valorUnitario the valorUnitario to set
+     */
+    public void setValorUnitario(String valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public String getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(String valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the fornecedor
+     */
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    /**
+     * @param fornecedor the fornecedor to set
+     */
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    private String nome;
+    private String descricao;
+    private String valorUnitario;
+    private String quantidade;
+    private String valorTotal;
+    private String fornecedor;
+    
+}
